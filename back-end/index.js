b27077aeddf8b2f4ab9b99935d20a785188664a8
brunomaschietto <bruno4_10@hotmail.com>
2024-09-20 16:14:50 -0300

    Mudando a porta da aplicacao

--- back-end/index.js
+++ back-end/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors())
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/api/noticias', async function (req, res) {
     try {
