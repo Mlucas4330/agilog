bc830b74759c96e21e3b801466ea54d24901b34a
brunomaschietto <bruno4_10@hotmail.com>
2024-10-03 15:08:58 -0300

    Ajuste no script automatico do linux

--- back-end/run_script.sh
+++ back-end/run_script.sh
@@ -2,4 +2,4 @@ SCRIPT_DIR="/var/agilog/agilog/back-end"
 
 cd $SCRIPT_DIR
 
-timeout 600 /root/.nvm/versions/node/v20.11.1/bin/node buscaNoticiaDiaria.js
+timeout 60000 /root/.nvm/versions/node/v20.11.1/bin/node buscaNoticiaDiaria.js
